e442d4dbbf940414c3190bf994fac0aa175df45f
sjy9412 <sjy9412@gmail.com>
2020-05-15 12:34:52 +0900

    S02P31B205-43 | kakao Login

--- damda/app/src/main/java/com/example/damda/LoginActivity.kt
+++ damda/app/src/main/java/com/example/damda/LoginActivity.kt
@@ -1,9 +1,20 @@
 package com.example.damda
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
+import com.kakao.auth.ISessionCallback
+import com.kakao.auth.Session
+import com.kakao.network.ErrorResult
+import com.kakao.usermgmt.UserManagement
+import com.kakao.usermgmt.callback.MeV2ResponseCallback
+import com.kakao.usermgmt.response.MeV2Response
+import com.kakao.util.OptionalBoolean
+import com.kakao.util.exception.KakaoException
 import kotlinx.android.synthetic.main.activity_login.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -13,12 +24,13 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class LoginActivity : AppCompatActivity() {
     var login:Login? = null
+    private var callback: SessionCallback = SessionCallback()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
         var retrofit = Retrofit.Builder()
-            .baseUrl("https://loacalhost:8000/damda/login")
+            .baseUrl("https://i02b105.p.ssafy.io")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         var loginService: LoginService = retrofit.create(LoginService::class.java)
@@ -46,5 +58,54 @@ class LoginActivity : AppCompatActivity() {
             })
 
         }
+        Session.getCurrentSession().addCallback(callback)
+    }
+    private inner class SessionCallback : ISessionCallback {
+        override fun onSessionOpened() {
+            // 로그인 세션이 열렸을 때
+            UserManagement.getInstance().me( object : MeV2ResponseCallback() {
+                override fun onSuccess(result: MeV2Response?) {
+                    // 로그인이 성공했을 때
+                    var intent = Intent(this@LoginActivity, MainActivity::class.java)
+                    intent.putExtra("name", result!!.getNickname())
+                    intent.putExtra("profile", result!!.getProfileImagePath())
+                    if(result.getKakaoAccount().hasEmail() == OptionalBoolean.TRUE)
+                        intent.putExtra("email", result.getKakaoAccount().getEmail())
+                    else
+                        intent.putExtra("email", "none")
+                    if(result.getKakaoAccount().hasBirthday() == OptionalBoolean.TRUE)
+                        intent.putExtra("birthday", result.getKakaoAccount().getBirthday());
+                    else
+                        intent.putExtra("birthday", "none")
+                    startActivity(intent)
+                    finish()
+                }
+
+                override fun onSessionClosed(errorResult: ErrorResult?) {
+                    // 로그인 도중 세션이 비정상적인 이유로 닫혔을 때
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "세션이 닫혔습니다. 다시 시도해주세요 : ${errorResult.toString()}",
+                        Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
+        override fun onSessionOpenFailed(exception: KakaoException?) {
+            // 로그인 세션이 정상적으로 열리지 않았을 때
+            if (exception != null) {
+                com.kakao.util.helper.log.Logger.e(exception)
+                Toast.makeText(
+                    this@LoginActivity,
+                    "로그인 도중 오류가 발생했습니다. 인터넷 연결을 확인해주세요 : $exception",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    private fun redirectSignupActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
