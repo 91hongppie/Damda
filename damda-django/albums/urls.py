1f9c0d2a27161aea71e5ad1ee2d077cd4f8a1bd4
조규홍 <marioz9@naver.com>
2020-05-26 05:25:39 +0000

    Update urls.py

--- damda-django/albums/urls.py
+++ damda-django/albums/urls.py
@@ -7,7 +7,8 @@ app_name = "albums"
 urlpatterns = [
     path('photo/<int:family_pk>/<int:album_pk>/', views.photo),
     path('photo/<int:family_pk>/', views.all_photo),
-    path('<int:family_pk>/', views.album),
+    path('<int:family_pk>/', views.albums),
+    path('album/<int:album_pk>/', views.album),
     path('photo/', views.photo_delete),
     path('<int:family_pk>/face/', views.face)
 ]
