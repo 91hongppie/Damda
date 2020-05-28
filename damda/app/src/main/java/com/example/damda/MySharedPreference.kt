f4e915c57c09c866f7e6b245f465b61ce3abc162
EUNBI203 <unbi203@gmail.com>
2020-05-28 22:22:23 +0900

    S02P31B205-98 | Add auto upload

--- damda/app/src/main/java/com/example/damda/MySharedPreference.kt
+++ damda/app/src/main/java/com/example/damda/MySharedPreference.kt
@@ -4,14 +4,14 @@ import android.content.Context
 import android.content.SharedPreferences
 
 class MySharedPreferences(context: Context) {
-
     val PREFS_FILENAME = "prefs"
     val PREF_KEY_MY_EDITTEXT = "token"
     val prefs: SharedPreferences = context.getSharedPreferences(PREFS_FILENAME, 0)
     /* 파일 이름과 EditText를 저장할 Key 값을 만들고 prefs 인스턴스 초기화 */
-   var token: String?
+    var token: String?
         get() = prefs.getString(PREF_KEY_MY_EDITTEXT, "")
         set(value) = prefs.edit().putString(PREF_KEY_MY_EDITTEXT, value).apply()
+
     /* get/set 함수 임의 설정. get 실행 시 저장된 값을 반환하며 default 값은 ""
      * set(value) 실행 시 value로 값을 대체한 후 저장 */
     var user_id: String?
@@ -25,4 +25,12 @@ class MySharedPreferences(context: Context) {
     var state: String?
         get() = prefs.getString("state", "")
         set(value) = prefs.edit().putString("state", value).apply()
+
+    var autoStatus: Boolean
+        get() = prefs.getBoolean("autoStatus", true)
+        set(value) = prefs.edit().putBoolean("autoStatus", value).apply()
+
+    var autoId: String?
+        get() = prefs.getString("autoId", null)
+        set(value) = prefs.edit().putString("autoId", value).apply()
 }
--- damda/app/src/main/java/com/example/damda/activity/MainActivity.kt
+++ damda/app/src/main/java/com/example/damda/activity/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.example.damda.activity
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.media.ExifInterface
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.MenuItem
 import android.view.View
@@ -11,6 +18,7 @@ import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.RelativeLayout
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -36,8 +44,12 @@ import com.example.damda.GlobalApplication.Companion.prefs
 import com.example.damda.R
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.iid.FirebaseInstanceId
+import java.io.File
+import java.io.FileNotFoundException
 import java.io.IOException
 import java.net.URL
+import java.net.UnknownHostException
+import java.util.*
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     var mBackWait:Long = 0
@@ -45,6 +57,11 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottom_navigation.setOnNavigationItemSelectedListener(this)
+        val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+        if(activeNetwork?.type == ConnectivityManager.TYPE_WIFI && prefs.autoStatus){
+            checkMidea()
+        }
         if (navStatus == 1) {
             bottom_navigation.layoutParams.height = 0
         } else {
@@ -64,7 +81,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
                 // Get new Instance ID token
                 val token = task.result?.token
-                sendRegistrationToServer(token!!)
+               sendRegistrationToServer(token!!)
                 // Log and toast
                 val msg = getString(R.string.msg_token_fmt, token)
                 Log.d(TAG, msg)
@@ -185,5 +202,127 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         }else {
             super.onBackPressed()
         }
+    }
+    @SuppressLint("Recycle")
+    fun checkMidea() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED )
+        {
+            val imageProjection =
+                arrayOf(MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID)
+            val lastId = prefs.autoId
+            val imageCursor = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, imageProjection, null, null, MediaStore.Images.Media._ID +  " desc ")
+            var images = arrayListOf<File>()
+            var paths = arrayListOf<String>()
+            val imageIdIndex = imageCursor?.getColumnIndex(MediaStore.Images.Media._ID)
+            val imageDataIndex = imageCursor?.getColumnIndex(MediaStore.Images.Media.DATA)
+            if (imageCursor != null && imageCursor.count > 0) {
+                imageCursor.moveToFirst()
+                prefs.autoId = imageCursor.getString(imageIdIndex!!)
+                while (true) {
+                    val imageId = imageCursor.getString(imageIdIndex)
+                    val imageData = imageCursor.getString(imageDataIndex!!)
+                    // 최종 동기화 아이디보다 이전 아이디일 경우 중지
+                    if (!isValidDate(lastId, imageId)) {
+                        break
+                    }
+                    paths.add(imageData)
+                    val image = File(imageData)
+                    try {
+                        images.add(image)
+                    } catch (e: FileNotFoundException) {
+                        e.printStackTrace()
+                    }
+
+                    if (!imageCursor.moveToNext()) {
+                        break
+                    }
+                }
+                val url = URL("http://10.0.2.2:8000/api/albums/addphoto/")
+                if (images.size > 0) {
+                    var dialogBuilder = android.app.AlertDialog.Builder(this)
+                    dialogBuilder.setTitle("사진 업로드")
+                    dialogBuilder.setMessage("새로운 사진이 있습니다.\n업로드 하시겠습니까?")
+                    var dialogListener = object: DialogInterface.OnClickListener{
+                        override fun onClick(dialog: DialogInterface?, which: Int) {
+                            when(which){
+                                DialogInterface.BUTTON_NEUTRAL -> {
+                                    uploadImage(url, images, paths)
+                                }
+                            }
+                        }
+                    }
+                    dialogBuilder.setPositiveButton("나중에 하기", dialogListener)
+                    dialogBuilder.setNeutralButton("지금 업로드", dialogListener)
+                    dialogBuilder.show()
+                }
+            }
+        }
+        return
+    }
+
+    fun uploadImage(url : URL, images: ArrayList<File>, paths: ArrayList<String>) {
+        try {
+            val MEDIA_TYPE_IMAGE = MediaType.parse("image/*")
+
+            val requestBody = MultipartBody.Builder().setType(MultipartBody.FORM)
+                .addFormDataPart("user_id", "${prefs.user_id}")
+
+            for (i in 0 until images.size) {
+                val exif = ExifInterface(paths[i])
+                var date: String
+                var time: String
+                if (exif.getAttribute(ExifInterface.TAG_DATETIME) != null) {
+                    val datetime = exif.getAttribute(ExifInterface.TAG_DATETIME)
+                    val datetime_split = datetime.split(" ")
+                    date = datetime_split[0].split(":").joinToString("")
+                    time = datetime_split[1].split(":").joinToString("")
+                } else {
+                    val today = Date()
+                    date = today.year.toString() + today.month.toString() + today.day.toString()
+                    time = today.hours.toString() + today.minutes.toString() + today.seconds.toString()
+                }
+                requestBody.addFormDataPart("uploadImages", "damda_${prefs.user_id}_${date}_${time}", RequestBody.create(MEDIA_TYPE_IMAGE, images[i]))
+            }
+
+            val body = requestBody.build()
+            val jwt = GlobalApplication.prefs.token
+            val request = Request.Builder().addHeader("Authorization", "JWT $jwt")
+                .url(url)
+                .post(body)
+                .build()
+
+            val client = OkHttpClient()
+
+            val callback = Callback1()
+
+            client.newCall(request).enqueue(callback)
+
+        } catch (e: UnknownHostException) {
+            Log.d("Error", "$e")
+        } catch (e: Exception) {
+            Log.d("Exception", "$e")
+        }
+    }
+
+    inner class Callback1 : Callback {
+        override fun onFailure(call: okhttp3.Call, e: IOException) {
+            Log.d("Sever response", "error: $e")
+
+        }
+
+        override fun onResponse(call: okhttp3.Call, response: Response) {
+            val status = response.code()
+            Log.d("server response", "$status")
+
+            val result = response.body()?.string()
+
+            Log.d("Sever response", "result: $result")
+        }
+    }
+    private fun isValidDate(lastId: String?, Id: String): Boolean {
+        if (lastId != null) {
+            return lastId.compareTo(Id) < 0
+        }
+        return false
     }
 }
